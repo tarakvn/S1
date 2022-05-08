ead0cf364fb48dde058eae72f5ffdbc460ceb497
tarakavian84@gmail.com <tarakavian84@gmail.com>
2022-05-08 19:11:15 +0430

    Add project files.

---
+++ S1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int N = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your number is: " + N);
+            Console.ReadKey();
+        }
+    }
+}
